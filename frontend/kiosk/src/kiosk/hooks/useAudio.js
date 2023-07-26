c4ab23912fa5dda80246b8eb65daba1040dcf36b
한승준 <tmdwns950428@naver.com>
2023-07-26 23:36:43 +0900

    refactor: audio 관련 핵심 로직 주석 달기

--- frontend/kiosk/src/kiosk/hooks/useAudio.js
+++ frontend/kiosk/src/kiosk/hooks/useAudio.js
@@ -3,37 +3,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { setAudio, setPlaying } from "../redux/actions/audioActions";
 
 const useAudio = (url, location) => {
-  const audio = useSelector((state) => state.audio.audio);
-  const playing = useSelector((state) => state.audio.playing);
-  const dispatch = useDispatch();
+  const audio = useSelector((state) => state.audio.audio); // Redux state에서 audio 객체를 가져옵니다.
+  const playing = useSelector((state) => state.audio.playing); // Redux state에서 audio 재생 상태를 가져옵니다.
+  const dispatch = useDispatch(); // Redux 액션을 dispatch하기 위한 함수입니다.
 
-  const toggle = () => dispatch(setPlaying(!playing));
+  const toggle = () => dispatch(setPlaying(!playing)); // 재생 상태를 변경하는 함수입니다.
 
   useEffect(() => {
+    // 재생 상태나 audio 객체가 변경될 때마다 실행됩니다.
     if (playing && audio) {
-      audio.currentTime = 0;
-      audio.play();
+      audio.currentTime = 0; // 오디오 재생 위치를 초기화합니다.
+      audio.play(); // 오디오를 재생합니다.
     } else if (!playing && audio) {
-      audio.pause();
+      audio.pause(); // 오디오를 일시 정지합니다.
     }
   }, [playing, audio]);
 
   useEffect(() => {
+    // audio 객체가 변경될 때마다 실행됩니다.
     if (audio) {
+      // 오디오가 끝나면 재생 상태를 false로 변경합니다.
       audio.addEventListener("ended", () => dispatch(setPlaying(false)));
       return () => {
+        // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거합니다.
         audio.removeEventListener("ended", () => dispatch(setPlaying(false)));
       };
     }
   }, [audio, dispatch]);
 
   useEffect(() => {
-    const newAudio = new Audio(url);
-    dispatch(setAudio(newAudio));
-    dispatch(setPlaying(false));
+    // url이나 dispatch 함수가 변경될 때마다 실행됩니다.
+    const newAudio = new Audio(url); // 새로운 Audio 객체를 생성합니다.
+    dispatch(setAudio(newAudio)); // Redux state에 새로운 Audio 객체를 저장합니다.
+    dispatch(setPlaying(false)); // 재생 상태를 false로 변경합니다.
   }, [url, dispatch]);
 
-  return [playing, toggle];
+  return [playing, toggle]; // 재생 상태와 재생 상태를 변경하는 함수를 반환합니다.
 };
 
 export default useAudio;
